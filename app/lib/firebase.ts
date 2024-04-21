3d9123fd57b711a98c07b66cd9788ce625f7755f
Jonathan Gamble <jdgamble555@gmail.com>
2024-04-21 14:55:19 -0500

    test

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -2,6 +2,8 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+const isBrowser = typeof window !== 'undefined';
+
 declare global {
     interface Window {
         ENV: {
@@ -12,14 +14,10 @@ declare global {
 
 export const useFirebase = () => {
 
-    if (process.env.PUBLIC_FIREBASE_CONFIG) {
-        return {
-            db: null,
-            auth: null
-        }
-    }
+    const firebase_config = isBrowser
+        ? window.ENV.PUBLIC_FIREBASE_CONFIG
+        : JSON.parse(process.env.PUBLIC_FIREBASE_CONFIG!);
 
-    const firebase_config = window.ENV.PUBLIC_FIREBASE_CONFIG;
     const app = getApps().length
         ? getApp()
         : initializeApp(firebase_config);
